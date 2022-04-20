478cbc0279e2d74a40d3667b07bc5085dd819331
althoof <muhammadalthoof@gmail.com>
2022-04-20 21:49:54 +0700

    Mengubah fungsi
    saya menemukan bad smells pada line 55 yang merupakan long methode dan terjadi perubahan pada setMonthlySalary.

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -52,24 +52,29 @@ public class Employee {
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
 	
-	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
+	public void setMonthlySalary(int grade){	
+		static void calculateTax(double ti) {
+			double tax=0,cess=0;
+		if(ti > 3000000 && ti < 7000000) 
+			tax = (ti-450000)*1.5; 
+	    else if(ti > 500000 && ti < 7000000) 
+			tax = 450000 + (ti-50000)*1.5; 
+	    else if(ti > 7000000) 
+		  tax = 450000 + (ti-7000000)*1.5;
+
+		  cess = tax * 0.05;
+			System.out.println("Total tax: "+tax+" Total cess: "+cess);
+			System.out.println("Tax payable:"+(tax+cess));
+		}
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Enter Total Income: ");
+			double total_income = sc.nextDouble();
+			calculateTax(total_income);
+			sc.close();
 		}
 	}
+}
 	
 	public void setAnnualDeductible(int deductible) {	
 		this.annualDeductible = deductible;
